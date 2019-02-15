729a3a6e09305ed59142997646ce2708fde0d7ff
Dmitrij Fedorenko <job@fedorenko-d.ru>
2019-02-15 17:36:27 +0300

    Init commit

---
+++ proxyprotocol.go
@@ -0,0 +1,4 @@
+// Package proxyprotocol impleement HA ProxyProtocol V1 and V2 for receiver
+//
+// Prorxyprotocol spec http://www.haproxy.org/download/2.0/doc/proxy-protocol.txt
+package proxyprotocol
