24ccb506b9483a71790be061bfadf91279a5f7ba
Dmitrij Fedorenko <job@fedorenko-d.ru>
2019-02-26 09:43:01 +0300

    Add tests for Listener

--- listener_test.go
+++ listener_test.go
@@ -1,6 +1,8 @@
 package proxyprotocol_test
 
 import (
+	"errors"
+	"net"
 	"reflect"
 	"testing"
 
@@ -24,4 +26,194 @@ func TestNewListener(t *testing.T) {
 	if !reflect.DeepEqual(expectedListener, listener) {
 		t.Errorf("NewListener return unexpected result. Expected %#v, got %#v.", expectedListener, listener)
 	}
+}
+
+func TestListener_WithLogger(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	rawListener := NewMockListener(mockCtrl)
+
+	listener := proxyprotocol.NewListener(rawListener)
+
+	withLogger := listener.WithLogger(t.Logf)
+
+	if reflect.ValueOf(withLogger.LoggerFn).Pointer() != reflect.ValueOf(t.Logf).Pointer() {
+		t.Errorf("Unexpected LoggerFn")
+	}
+}
+
+func TestListener_WithSourceChecker(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	rawListener := NewMockListener(mockCtrl)
+
+	listener := proxyprotocol.NewListener(rawListener)
+
+	sourceChecker := func(net.Addr) (bool, error) {
+		return false, nil
+	}
+
+	withSourceChecker := listener.WithSourceChecker(sourceChecker)
+
+	if reflect.ValueOf(withSourceChecker.SourceChecker).Pointer() != reflect.ValueOf(sourceChecker).Pointer() {
+		t.Errorf("Unexpected SourceChecker")
+	}
+}
+
+func TestListener_WithHeaderParserBuilder(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	rawListener := NewMockListener(mockCtrl)
+
+	listener := proxyprotocol.NewListener(rawListener)
+
+	binaryHeaderParserBuilder := new(proxyprotocol.BinaryHeaderParserBuilder)
+	withHeaderParserBuilder := listener.WithHeaderParserBuilder(binaryHeaderParserBuilder)
+
+	if withHeaderParserBuilder.HeaderParserBuilder != binaryHeaderParserBuilder {
+		t.Errorf(
+			"Unexpected HeaderParserBuilder. Expect %s, got %s.",
+			binaryHeaderParserBuilder,
+			withHeaderParserBuilder.HeaderParserBuilder,
+		)
+	}
+}
+
+func TestListener_Close(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	rawListener := NewMockListener(mockCtrl)
+
+	listener := proxyprotocol.NewListener(rawListener)
+
+	expectedErr := errors.New("Closer error")
+	rawListener.EXPECT().Close().Return(expectedErr)
+
+	err := listener.Close()
+	if expectedErr != err {
+		t.Errorf("Unexpected close result. Expect %s, got %s", expectedErr, err)
+	}
+}
+
+func TestListener_Addr(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	rawListener := NewMockListener(mockCtrl)
+
+	listener := proxyprotocol.NewListener(rawListener)
+
+	expectedAddr := &net.TCPAddr{
+		IP:   net.IPv4(1, 2, 3, 4),
+		Port: 8080,
+	}
+
+	rawListener.EXPECT().Addr().Return(expectedAddr)
+
+	addr, ok := listener.Addr().(*net.TCPAddr)
+	if !ok || !expectedAddr.IP.Equal(addr.IP) || expectedAddr.Port != addr.Port {
+		t.Errorf("Unexpected addr result. Expect %s, got %s", expectedAddr, addr)
+	}
+}
+
+func TestListener_Accept(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	rawListener := NewMockListener(mockCtrl)
+
+	listener := proxyprotocol.NewListener(rawListener)
+
+	t.Run("when raw listener accept return error", func(t *testing.T) {
+		acceptErr := errors.New("Accept error")
+		rawListener.EXPECT().Accept().Return(nil, acceptErr)
+
+		conn, err := listener.Accept()
+
+		if nil != conn {
+			t.Errorf("Expect nil conn, but got %s", conn)
+		}
+
+		if err != acceptErr {
+			t.Errorf("Unexpected error. Expect %s, got %s", acceptErr, err)
+		}
+	})
+
+	t.Run("when raw listner accept return connection", func(t *testing.T) {
+		rawConn := NewMockConn(mockCtrl)
+		rawListener.EXPECT().Accept().Return(rawConn, nil).AnyTimes()
+
+		remoteAddr := net.TCPAddr{
+			IP:   net.IPv4(1, 2, 3, 4),
+			Port: 12345,
+		}
+		rawConn.EXPECT().RemoteAddr().Return(&remoteAddr).AnyTimes()
+
+		t.Run("listener not have SourceChecker", func(t *testing.T) {
+			conn, err := listener.Accept()
+
+			if nil != err {
+				t.Errorf("Expect nil error, but got %s", err)
+			}
+
+			if nil == conn {
+				t.Errorf("Expect conn not equal nil")
+			}
+		})
+
+		t.Run("listener have SourceChecker", func(t *testing.T) {
+			var sourceCheckErr error
+			var sourceCheckResult bool
+			sourceChecker := func(net.Addr) (bool, error) {
+				return sourceCheckResult, sourceCheckErr
+			}
+			listener = listener.WithSourceChecker(sourceChecker)
+
+			t.Run("source checker return error", func(t *testing.T) {
+				sourceCheckErr = errors.New("Source check err")
+				sourceCheckResult = false
+
+				conn, err := listener.Accept()
+				if err != sourceCheckErr {
+					t.Errorf("Unexpected error %s", err)
+				}
+
+				if nil != conn {
+					t.Errorf("Unexpeced connection %s", conn)
+				}
+			})
+
+			t.Run("source checker return false", func(t *testing.T) {
+				sourceCheckErr = nil
+				sourceCheckResult = false
+
+				conn, err := listener.Accept()
+				if nil != err {
+					t.Errorf("Unexpected error %s", err)
+				}
+
+				if rawConn != conn {
+					t.Errorf("Unexpected connection %s", conn)
+				}
+			})
+
+			t.Run("source checker return true", func(t *testing.T) {
+				sourceCheckErr = nil
+				sourceCheckResult = true
+
+				conn, err := listener.Accept()
+				if nil != err {
+					t.Errorf("Unexpected error %s", err)
+				}
+
+				if nil == conn {
+					t.Errorf("Unexpected connection %s", conn)
+				}
+			})
+		})
+	})
 }
